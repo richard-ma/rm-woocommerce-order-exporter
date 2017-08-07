37453431232fb5bf5c83722eef131c97636e24a6
Richard Ma <richard.ma.19850509@gmail.com>
2017-08-07 19:35:24 +0800

    add submenu for admin and exporter form

--- rm-woocommerce-order-exporter.php
+++ rm-woocommerce-order-exporter.php
@@ -1,11 +1,13 @@
 <?php
 /*
- * Plugin Name: Woocommerce order exporter
+ * Plugin Name: RM Woocommerce order exporter
  * Plugin_URI: https://github.com/richard-ma/rm-woocommerce-order-exporter
  * Author: Richard Ma
  * Author URI: http://richardma.info
  * Version: 1.0
  * Lincense: MIT
+ *
+ * Plugin Prefix: rmoe_ / RMOE_
  */
 
 // exit if accessed directly
@@ -18,6 +20,35 @@ if (!in_array( 'woocommerce/woocommerce.php', apply_filters('active_plugins', ge
     exit;
 }
 
-// require other php file
+// require other php files
+require_once( plugin_dir_path(__FILE__) . 'rm-functions.php');
 require_once( plugin_dir_path(__FILE__) . 'rm-woocommerce-order-exporter-csv.php');
 require_once( plugin_dir_path(__FILE__) . 'rm-woocommerce-order-exporter-excel.php');
+
+// require js & css files
+function rmoe_admin_enqueue_scripts() {
+    wp_enqueue_script('ajax-download', plugins_url('js/ajax-download', __FILE__), array('jquery'), 20170807, true);
+}
+add_action('admin_enqueue_scripts', 'rmoe_admin_enqueue_scripts');
+
+$RMOE_EXPORTER_FORM = plugin_dir_path(__FILE__) . 'rm-woocommerce-order-exporter-form.html';
+
+// add submenu export to woocommerce
+function rmoe_add_exporter_page_to_admin_submenu() {
+    add_submenu_page(
+        'woocommerce',
+        __('RM Woocommerce Order Exporter'),
+        __('Order Exporter'),
+        'manage_options',
+        'rmoe_order_exporter',
+        'rmoe_admin_exporter_submenu_callback'
+    );
+}
+add_action('admin_menu', 'rmoe_add_exporter_page_to_admin_submenu');
+
+// show order exporter form
+function rmoe_admin_exporter_submenu_callback() {
+    global $RMOE_EXPORTER_FORM;
+    echo file_get_contents($RMOE_EXPORTER_FORM);
+}
+
