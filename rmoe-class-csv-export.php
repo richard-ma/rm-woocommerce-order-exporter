8b32583dd7c9d8585500f49a4c75aa370d4d20fd
richard-ma <richard.ma.19850509@gmail.com>
2022-12-09 10:21:52 +0800

    remove commas which one at the end of line

--- rmoe-class-csv-export.php
+++ rmoe-class-csv-export.php
@@ -12,14 +12,14 @@ class RMOE_CsvExport extends RMOE_Export
         $output = "OrderNo, Name, Address, City, Province, Post, Country, Tel" . PHP_EOL;
 
         foreach($data as $item) {
-            $output .= $item['id'].',';
-            $output .= $item['name'].',';
-            $output .= $item['address'].',';
-            $output .= $item['city'].',';
-            $output .= $item['province'].',';
-            $output .= $item['post'].',';
-            $output .= $item['country'].',';
-            $output .= $item['tel'].',';
+            $output .= rtrim($item['id'], ", ").',';
+            $output .= rtrim($item['name'], ", ").',';
+            $output .= rtrim($item['address'], ", ").',';
+            $output .= rtrim($item['city'], ", ").',';
+            $output .= rtrim($item['province'], ", ").',';
+            $output .= rtrim($item['post'], ", ").',';
+            $output .= rtrim($item['country'], ", ").',';
+            $output .= rtrim($item['tel'], ", ").',';
             $output .= PHP_EOL;
         }
 
